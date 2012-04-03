448ae34f01e19a369e2ab52280ba56a41b9c2e6c
cloudzhou <cloudzhou@163.com>
2012-04-03 10:32:11 +0800

    add memcache support

--- settings.py
+++ settings.py
@@ -21,7 +21,7 @@ DATABASES = {
 }
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
         'LOCATION': '127.0.0.1:11211',
     }
 }
--- urls.py
+++ urls.py
@@ -2,8 +2,8 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('gitshell',
     url(r'^/?$', 'auth.views.home'),
-    url(r'^auth/fp/(\S+)/?$', 'auth.views.fingerprint'),
-    url(r'^dist/pj/(\S+)/?$', 'dist.views.project'),
-    url(r'^dist/refresh/?$', 'dist.views.refresh'),
-    url(r'^dist/print/?$', 'dist.views.print_project_partition'),
+    url(r'^private/auth/fp/(\S+)/?$', 'auth.views.fingerprint'),
+    url(r'^private/dist/pj/(\S+)/?$', 'dist.views.project'),
+    url(r'^private/dist/refresh/?$', 'dist.views.refresh'),
+    url(r'^private/dist/print/?$', 'dist.views.print_project_partition'),
 )
