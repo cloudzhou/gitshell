06a80191bd92438c772eda42541be508f2fbc609
cloudzhou <cloudzhou@163.com>
2012-04-07 00:18:41 +0800

    add login page

--- gsuser/views.py
+++ gsuser/views.py
@@ -4,8 +4,19 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 def user(request, user_name):
-    print user_name
     response_dictionary = {'hello_world': 'hello world1'}
     return render_to_response('home.html',
+                          response_dictionary,
+                          context_instance=RequestContext(request))
+
+def login(request):
+    response_dictionary = {'hello_world': 'hello world1'}
+    return render_to_response('gsuser/login.html',
+                          response_dictionary,
+                          context_instance=RequestContext(request))
+
+def join(request):
+    response_dictionary = {'hello_world': 'hello world1'}
+    return render_to_response('gsuser/join.html',
                           response_dictionary,
                           context_instance=RequestContext(request))
--- urls.py
+++ urls.py
@@ -6,11 +6,11 @@ urlpatterns = patterns('gitshell',
 
     # main
     url(r'^home/?$', 'index.views.home'),
-    url(r'^project/?$', 'index.views.home'),
+    url(r'^repos/?$', 'index.views.home'),
     url(r'^skills/?$', 'index.views.home'),
     url(r'^stats/?$', 'index.views.home'),
-    url(r'^login/?$', 'gsuser.views.user'),
-    url(r'^join/?$', 'gsuser.views.user'),
+    url(r'^login/?$', 'gsuser.views.login'),
+    url(r'^join/?$', 'gsuser.views.join'),
 
     # help
     url(r'^help/?$', 'help.views.default'),
@@ -22,9 +22,9 @@ urlpatterns = patterns('gitshell',
 
     # gitshell openssh keyauth and dist, private for subnetwork access by iptables, nginx port 9000
     url(r'^private/keyauth/fp/(\S+)/?$', 'keyauth.views.fingerprint'),
-    url(r'^private/dist/pj/(\S+)/?$', 'dist.views.project'),
+    url(r'^private/dist/pj/(\S+)/?$', 'dist.views.repos'),
     url(r'^private/dist/refresh/?$', 'dist.views.refresh'),
-    url(r'^private/dist/print/?$', 'dist.views.print_project_partition'),
+    url(r'^private/dist/print/?$', 'dist.views.print_repos_partition'),
 
     # write middleware to rewrite urlconf, by add 'urlconf' attribute to HttpRequest
     # gsuser
