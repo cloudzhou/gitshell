d9bf759f970051fa906b6b84ec275e2d4dd44248
cloudzhou <cloudzhou@163.com>
2012-04-21 05:03:05 +0800

    index page for all user

---
+++ skills/views.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-  
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+def skills(request):
+    response_dictionary = {'ii': range(0, 5), 'jj': range(0, 3), 'kk': range(0, 10)}
+    return render_to_response('skills/skills.html',
+                          response_dictionary,
+                          context_instance=RequestContext(request))
