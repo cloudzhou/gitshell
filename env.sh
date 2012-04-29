b0915b457e319fd6ec260e30ab25182f79f19649
cloudzhou <cloudzhou@163.com>
2012-04-30 00:24:47 +0800

    skills html init, 0.1, a lot of work now. it is a new line

---
+++ env.sh
@@ -0,0 +1,3 @@
+export PYTHONPATH=/opt/8001/
+export DJANGO_SETTINGS_MODULE=gitshell.settings
+ALTER DATABASE gitshelldb charset=utf8;
--- skills/views.py
+++ skills/views.py
@@ -4,9 +4,11 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from gitshell.gsuser.Forms import SkillsForm
 
 def skills(request):
-    response_dictionary = {'ii': range(0, 5), 'jj': range(0, 3), 'kk': range(0, 10)}
+    skillsForm = SkillsForm()
+    response_dictionary = {'ii': range(0, 5), 'jj': range(0, 3), 'kk': range(0, 10), 'skillsForm': skillsForm}
     return render_to_response('skills/skills.html',
                           response_dictionary,
                           context_instance=RequestContext(request))
