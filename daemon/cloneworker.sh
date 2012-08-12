b85836f2e549fb6d271dacabf5bd43f7bd16cf76
cloudzhou <cloudzhou@163.com>
2012-08-12 16:00:15 +0800

    add worker.sh start and stop method

---
+++ daemon/cloneworker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+cd $DIR; cd ../../
+GITSHELL_DIR=`pwd`
+
+export PYTHONPATH=$GITSHELL_DIR
+export DJANGO_SETTINGS_MODULE=gitshell.settings
+
+. /lib/lsb/init-functions
+
+SCRIPT="$GITSHELL_DIR/gitshell/daemon/cloneworker.py"
+case "$1" in
+  start)
+    python "$SCRIPT" start
+    ;;
+  stop)
+    python "$SCRIPT" stop
+    ;;
+  *)
+	log_action_msg "Usage: ${SCRIPT} {start|stop}"
+	exit 1
+esac
