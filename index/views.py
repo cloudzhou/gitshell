23485b4be2d675f4178bf7ad6caa43b4c6a005f0
cloudzhou <cloudzhou@163.com>
2012-08-19 23:57:10 +0800

    give me one week, things will be done

--- index/views.py
+++ index/views.py
@@ -1,9 +1,19 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from gitshell.repo.models import RepoManager
+from gitshell.gsuser.models import GsuserManager
+from gitshell.explore.views import get_hot_repo_ids
 
 def index(request):
-    response_dictionary = {'hello_world': 'hello world', 'ii': range(0, 3)}
+    repo_ids = get_hot_repo_ids()
+    repos = RepoManager.list_repo_by_ids(repo_ids)
+    user_ids = [x.user_id for x in repos]
+    users_dict = GsuserManager.map_users(user_ids)
+    username_dict = dict([(users_dict[x]['id'], users_dict[x]['username']) for x in users_dict])
+    userimgurl_dict = dict([(users_dict[x]['id'], users_dict[x]['imgurl']) for x in users_dict])
+    response_dictionary = {'repos': repos, 'users_dict': users_dict, 'username_dict': username_dict, 'userimgurl_dict': userimgurl_dict}
+    print repos, users_dict, username_dict
     return render_to_response('index.html',
                           response_dictionary,
                           context_instance=RequestContext(request))
