33e1def734b0b896ac5107b53f35d3e7532e1eff
cloudzhou <cloudzhou@163.com>
2013-06-05 22:04:20 +0800

    do not forget visibly = 0

--- objectscache/da.py
+++ objectscache/da.py
@@ -46,7 +46,7 @@ rawsql = {
     'recommend_l_userId':
         'select * from gsuser_recommend where visibly = 0 and user_id = %s order by modify_time desc limit %s, %s',
     'thirdpartyuser_s_userType_tpId':
-        'select * from gsuser_thirdpartyuser where user_type = %s and tp_id = %s',
+        'select * from gsuser_thirdpartyuser where visibly = 0 and user_type = %s and tp_id = %s',
     # repo #
     'repo_s_userId_name':
         'select * from repo_repo where visibly = 0 and user_id = %s and name = %s limit 0, 1',
