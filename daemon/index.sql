e264fe41f43f4d9a48cb5d32eff6f3c6a6bfa45b
cloudzhou <cloudzhou@163.com>
2013-06-27 18:01:40 +0800

    change index.sql to include issue, issuecomment

--- daemon/index.sql
+++ daemon/index.sql
@@ -15,11 +15,12 @@ create index repo_forkid on repo_repo (visibly, fork_repo_id)
 
 create index commithistory_rid_cid_idx on repo_commithistory (visibly, repo_id, commit_id);
 create index repomember_rid_uid on repo_repomember (visibly, repo_id, user_id);
-create index issues_cons_mtime_idx on repo_issues (visibly, repo_id, assigned, tracker, status, priority, modify_time desc);
-create index issues_rid_mtime_idx on repo_issues (visibly, repo_id, modify_time desc);
-create index issues_assigned_status_mtime_idx on repo_issues (visibly, assigned, status, modify_time desc);
 
-create index issuescomment_iid_ctime_idx on repo_issuescomment (visibly, issues_id, create_time asc);
+create index issue_cons_mtime_idx on issue_issue (visibly, repo_id, assigned, tracker, status, priority, modify_time desc);
+create index issue_rid_mtime_idx on issue_issue (visibly, repo_id, modify_time desc);
+create index issue_assigned_status_mtime_idx on issue_issue (visibly, assigned, status, modify_time desc);
+create index issuecomment_iid_ctime_idx on issue_issuecomment (visibly, issue_id, create_time asc);
+
 create index forkhistory_rid_mtime_idx on repo_forkhistory (visibly, repo_id, modify_time desc);
 create index watchhistory_rid_mtime_idx on repo_watchhistory (visibly, watch_repo_id, modify_time desc);
 create index scene_uid_idx on todolist_scene (visibly, user_id)
