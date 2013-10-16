f314bbe81b0435dcee1c40b507c522f2322fa2fd
Yuwen Song <yuwen.s512@gmail.com>
2013-10-16 09:20:52 +0800

    some bugs fixed

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,11 @@
 import os
 import sys
-import django.core.handlers.wsgi
 
-#sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+sys.path.append('/Library/Python/2.7/site-packages')
+sys.path.append('/System/Library/Frameworks/Python.framework/Versions/2.7/Extras/lib/python')
+print sys.path
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gitshell.settings")
+import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
 
